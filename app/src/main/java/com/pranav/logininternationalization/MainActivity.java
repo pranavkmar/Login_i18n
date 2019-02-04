27e5db83cff9b3a8fc6ff00faddc11f644264240
Pranav_BI_LAB <informpranav@gmail.com>
2019-02-04 16:46:25 +0530

    Add Strings

--- app/src/main/java/com/pranav/logininternationalization/MainActivity.java
+++ app/src/main/java/com/pranav/logininternationalization/MainActivity.java
@@ -1,7 +1,9 @@
 package com.pranav.logininternationalization;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
